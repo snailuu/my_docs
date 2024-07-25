88c22ae71960d3eafa70ad5fee5ea1b71d657ab2
snailuu <67217277@qq.com>
2024-07-25 09:45:44 +0800

    feat: 新增工作流

--- rspress.config.ts
+++ rspress.config.ts
@@ -2,17 +2,17 @@ import * as path from 'path';
 import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
-  root: path.join(__dirname, 'docs'),
-  title: '文档唠唠叨叨',
-  description: '记录开发过程中的唠唠叨叨',
-  icon: '/rspress-icon.png',
-  logo: {
-    light: '/rspress-light-logo.png',
-    dark: '/rspress-dark-logo.png',
-  },
-  themeConfig: {
-    socialLinks: [
-      { icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
-    ],
-  },
+	root: path.join(__dirname, 'docs'),
+	title: '文档唠唠叨叨',
+	description: '记录开发过程中的唠唠叨叨',
+	icon: '/rspress-icon.png',
+	logo: {
+		light: '/rspress-light-logo.png',
+		dark: '/rspress-dark-logo.png',
+	},
+	themeConfig: {
+		socialLinks: [
+			{ icon: 'github', mode: 'link', content: 'https://github.com/web-infra-dev/rspress' },
+		],
+	},
 });
